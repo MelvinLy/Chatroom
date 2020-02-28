3e4d845feb072ebd907c8ef49413c0991f777f06
melvinly <melvinly@gmail.com>
2020-02-28 18:15:52 -0500

    no message

--- Main.java
+++ Main.java
@@ -5,14 +5,18 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Server s = new Server(Integer.parseInt(args[2]));
+		String hostname = args[0];
+		String connecttoport = args[1];
+		String serverport = args[2];
+		String username = args[3];
+		Server s = new Server(Integer.parseInt(serverport));
 		s.start();
 		while(true) {
 			Scanner input = new Scanner(System.in);
 			try {
-				Client c = new Client(args[0], Integer.parseInt(args[1]));
+				Client c = new Client(hostname, Integer.parseInt(connecttoport));
 				String message = input.nextLine();
-				c.send(/*args[3] + ": " + */ message + "\n");
+				c.send(/*username + ": " + */ message + "\n");
 			}
 			catch(Exception e) {
 				
