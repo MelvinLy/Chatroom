68ae818a4b918415708ef35723b371cff7dfd0fe
melvinly <melvinly@gmail.com>
2020-03-11 20:59:26 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -84,7 +84,7 @@ public class GUI {
 					}
 				}
 			}
-		}.start();
+		}.run();
 		//Listen as client thread
 		new Thread() {
 			public void run() {
@@ -111,7 +111,7 @@ public class GUI {
 					}
 				}
 			}
-		}.start();
+		}.run();
 
 		//Listen as server thread
 		new Thread() {
@@ -147,7 +147,7 @@ public class GUI {
 					}
 				}
 			}
-		}.start();
+		}.run();
 
 	}
 
